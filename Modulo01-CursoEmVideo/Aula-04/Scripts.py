a6b47bf52f0471cc30d397c234047ae05e40f4f4
Dego Chiarelotto <diegocamiloaps@gmail.com>
2022-05-18 19:24:10 -0300

    1 Commit

---
+++ Modulo01-CursoEmVideo/Aula-04/Scripts.py
@@ -0,0 +1,4 @@
+nome = input('Qual e seu nome?')
+print('Olá '+nome+' Seja bem vindo(a) ao curso de python')
+dadosNas = input(nome+' Digite que dia que voce nesceu nesse formato DD/MM/AAAA: ')
+print('Seu nome e :'+nome+' Sua idade : '+dadosNas)
