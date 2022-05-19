e33076f7388a5e9fcca7171da2816f97ece4dc27
Dego Chiarelotto <100320958+diegochiarelotto@users.noreply.github.com>
2022-05-19 08:16:45 -0300

    Create Ex01.py

---
+++ Modulo01-CursoEmVideo/Aula-06/Ex01.py
@@ -0,0 +1,19 @@
+print('========Desafio 01===========')
+nome = input('Digite seu nome: ')
+print('Olá, ',nome, ' seja bem, vindo ao curso de Python.')
+
+print('========Desafio 02===========')
+print('Olá,',nome, ' por favor digite os dados do seu nascimento para continuarmos :)')
+diaNas = input('Dia:')
+mesNas = input('Mês:')
+anoNas = input('Ano:')
+
+print('Você nasceu no dia ',diaNas,' de ',mesNas,' no ano de ',anoNas,' confirma?')
+
+print('========Desafio 03===========')
+print('Vamos calcular? ',' a seguir digite os numeros para a soma')
+num1 = input('Primeiro Numero: ')
+num2 = input('Segundo Numero: ')
+soma= int(num1)+int(num2)
+print('A soma entre eles é : ',soma)
+
